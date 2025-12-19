f46a0a4d8f2740d7172aa825533e4d152e2cbb3e
Kleart10 <kleartadri1@outlook.com>
2025-12-19 22:37:36 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import bookRoutes from "./routes/books.js";
+import transactionRoutes from "./routes/transactions.js";
+import categoryRoutes from "./routes/categories.js";
+
+/* App Config */
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+/* Middlewares */
+app.use(express.json());
+app.use(cors());
+
+/* API Routes */
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/categories", categoryRoutes);
+
+/* MongoDB Connection (UPDATED) */
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("✅ MongoDB CONNECTED");
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB connection error:", error);
+  });
+
+/* Test Route */
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to LibraryApp");
+});
+
+/* Server Listen */
+app.listen(port, () => {
+  console.log(`🚀 Server is running on PORT ${port}`);
+});
